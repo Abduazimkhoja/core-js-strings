81739458ea60dabd7ee6d9130e4a85ac9e5ba5b9
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-11-18 01:32:01 +0500

    fix: used return instead of direct via arrowF

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -1,31 +1,57 @@
 // 15,2
 const getStringLength = (str = '') => (str ? str.length : 0);
 const isString = (str) => typeof str === 'string' || str instanceof String;
-const concatenateStrings = (a, b) => a + b;
-const getFirstChar = (str) => str.charAt(0);
-const removeLeadingAndTrailingWhitespaces = (str) => str.trim();
-const removeLeadingWhitespaces = (str) => str.trimStart();
-const removeTrailingWhitespaces = (str) => str.trimEnd();
-const repeatString = (str, count) => str.repeat(count > 0 ? count : 0);
-const removeFirstOccurrences = (str, remove) => str.replace(remove, '');
-const removeLastOccurrences = (str, remove) =>
-  str && str.includes(remove)
-    ? str.slice(0, str.lastIndexOf(remove)) +
-      str.slice(str.lastIndexOf(remove) + remove.length)
+const concatenateStrings = (a, b) => {
+  return a.concat(b);
+};
+const getFirstChar = (str) => {
+  return str.charAt(0);
+};
+const removeLeadingAndTrailingWhitespaces = (str) => {
+  return str.trim();
+};
+const removeLeadingWhitespaces = (str) => {
+  return str.trimStart();
+};
+const removeTrailingWhitespaces = (str) => {
+  return str.trimEnd();
+};
+const repeatString = (str, count) => {
+  return str.repeat(count > 0 ? count : 0);
+};
+const removeFirstOccurrences = (str, remove) => {
+  return str.indexOf(remove) !== -1
+    ? str.slice(0, str.indexOf(remove)) +
+        str.slice(str.indexOf(remove) + remove.length)
     : str;
-const sumOfCodes = (str) =>
-  str ? str.split('').reduce((a, b) => a + b.charCodeAt(), 0) : 0;
-const startsWith = (str, substr) => str.startsWith(substr);
-const endsWith = (str, substr) => str.endsWith(substr);
-const formatTime = (m, s) =>
-  `${`${m}`.padStart(2, '0')}:${`${s}`.padStart(2, '0')}`;
+};
+const removeLastOccurrences = (str, remove) => {
+  return str.lastIndexOf(remove) !== -1
+    ? str.slice(0, str.lastIndexOf(remove)) +
+        str.slice(str.lastIndexOf(remove) + remove.length)
+    : str;
+};
+const sumOfCodes = (str) => {
+  return str ? str.split('').reduce((a, b) => a + b.charCodeAt(), 0) : 0;
+};
+const startsWith = (str, substr) => {
+  return str.startsWith(substr);
+};
+const endsWith = (str, substr) => {
+  return str.endsWith(substr);
+};
+const formatTime = (m, s) => {
+  return `${`${m}`.padStart(2, '0')}:${`${s}`.padStart(2, '0')}`;
+};
 const reverseString = (str) => str.split('').reverse().join('');
 const orderAlphabetically = (str) =>
   str
     .split('')
     .sort((a, b) => a.localeCompare(b))
     .join('');
-const containsSubstring = (str, substr) => str.includes(substr);
+const containsSubstring = (str, substr) => {
+  return str.includes(substr);
+};
 const countVowels = (str) =>
   str
     .split('')
@@ -49,8 +75,8 @@ const reverseWords = (str) =>
     .split(' ')
     .map((item) => reverseString(item))
     .join(' ');
-const invertCase = (str) =>
-  str
+const invertCase = (str) => {
+  return str
     .split(' ')
     .map((item) =>
       item
@@ -63,11 +89,14 @@ const invertCase = (str) =>
         .join('')
     )
     .join(' ');
+};
 const getStringFromTemplate = (firstName, lastName) =>
   `Hello, ${firstName} ${lastName}!`;
 const extractNameFromTemplate = (str) => str.replace(/Hello, |!/g, '');
 const unbracketTag = (str) => str.replace(/<|>/g, '');
-const extractEmails = (str) => str.split(';');
+const extractEmails = (str) => {
+  return str.split(';');
+};
 const encodeToRot13 = (str) => {
   return str
     .split('')
